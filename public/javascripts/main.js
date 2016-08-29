f967c98849927e1b705b9c7fcfcec008edb32b7c
Dunbarry <RyanADunbar@gmail.com>
2016-08-29 16:21:45 -0600

    api online

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,9 @@
-$('.screen').click(function(){
+$('#submit').click(function(){
+  console.log("Drone fetch initiated.")
+  category=$('.dropdown').val();
+  query=$('.query').val();
   $.ajax({
-    url:'http://localhost:4242/Ronin',
+    url:'http://localhost:4242/'+category+'/'+query,
     error: function(err) {console.error(err)},
     method: 'GET',
     success: function(data){
