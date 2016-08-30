e8002542034beee7b04e4610d127a872d19e8e92
Dunbarry <RyanADunbar@gmail.com>
2016-08-29 18:18:53 -0600

    API online

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,13 @@
 $('#submit').click(function(){
   console.log("Drone fetch initiated.")
-  category=$('.dropdown').val();
-  query=$('.query').val();
+  category="/"+$('.dropdown').val();
+  query="/"+$('.query').val();
+  if(category==="/name"){
+    category=query;
+    query="";
+  }
   $.ajax({
-    url:'http://localhost:4242/'+category+'/'+query,
+    url:'http://localhost:4242'+category+query,
     error: function(err) {console.error(err)},
     method: 'GET',
     success: function(data){
