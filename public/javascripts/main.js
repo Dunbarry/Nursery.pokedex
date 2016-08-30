728ef799ca5db08139c1d390598b2979d9a7fa0b
Dunbarry <RyanADunbar@gmail.com>
2016-08-29 20:27:16 -0600

    Info in

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,21 @@
+function screenClear(){
+  $('.screen').html(
+    '<div class="dataContain">\
+    </div>\
+    <div class="searchContain">\
+      <select class="dropdown" type="dropdown">\
+        <option value="" disabled selected hidden>-Select Criteria-</option>\
+        <option value="name">Name</option>\
+        <option value="type">Type</option>\
+        <option value="species">Species</option>\
+        <option value="IV">IVs</option>\
+      </select>\
+      <input class="query" type="text" placeholder="query"></input>\
+      <input type="submit" id="submit" value="Search">\
+    </div>'
+  )
+}
+
 $('#submit').click(function(){
   console.log("Drone fetch initiated.")
   category="/"+$('.dropdown').val();
@@ -6,12 +24,36 @@ $('#submit').click(function(){
     category=query;
     query="";
   }
+  screenClear();
   $.ajax({
     url:'http://localhost:4242'+category+query,
     error: function(err) {console.error(err)},
     method: 'GET',
     success: function(data){
-      console.log(data);
+      console.log(data)
+      findings=JSON.stringify(data);
+      $('.dataContain').text(findings);
     }
   })
 })
+
+
+
+// <nav>
+//   <span>Home</span>
+//   <span>About</span>
+//   <span>Gallery</span>
+// </nav>
+// <h1>Welcome to {{title}}</h1>
+// <p>This site will allow any user to to dynamically search through documentation of the nursery's current inhabitants. To search the Pokedex, please select your criteria from the dropdown, then enter your query.</p>
+// <div class="searchContain">
+//   <p id="insturctions">To search by please enter your query formated as a range following this format without quotation: "30/40".</p>
+//   <select class="dropdown" type="dropdown">
+//     <option value="" disabled selected hidden>-Select Criteria-</option>
+//     <option value="name">Name</option>
+//     <option value="type">Type</option>
+//     <option value="species">Species</option>
+//     <option value="IV">IVs</option>
+//   </select>
+//   <input class="query" type="text" placeholder="query"></input>
+//   <input type="submit" id="submit" value="Search">
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var myPokedex= require('../lib/pokedex.js');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'My Pokedex', myPokedex:myPokedex });
-  console.log(myPokedex);
+  // console.log(myPokedex);
 });
 
 module.exports = router;
